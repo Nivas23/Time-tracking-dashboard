65c8cfd644d1529203f613e62d645046b1f6ab61
Nivas <tridentniva001@gmail.com>
2024-01-11 20:10:07 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const port = 3000;
 const data = require("./data.json");
 app.use(cors());
 app.get("/", (req, res) => {
+  res.setHeader('Content-Type', 'text/json');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.json(data);
 });
 
