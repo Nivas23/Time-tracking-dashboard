9011b6d3644beba629c436f317084cac646e1487
Nivas <tridentniva001@gmail.com>
2024-01-11 19:49:10 +0530

    fix

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function option() {
   fetch("http://localhost:3000/")
     .then((response) => response.json())
     .then((json) => {
-      console.log(json);
+      // console.log(json);
       data = json;
       if (choice === "daily") {
         daily++;
